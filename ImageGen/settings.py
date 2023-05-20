29400ca372ba5ed3cc6ace8f5f6512484d40bf6e
Awusa Collins <awusacollins@gmail.com>
2023-05-20 15:56:39 +0300

    new changes

--- ImageGen/settings.py
+++ ImageGen/settings.py
@@ -182,3 +182,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://web-production-f5fa1.up.railway.app/']
